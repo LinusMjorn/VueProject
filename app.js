83688cb80efa54e1b05689151bf73b49c5f8e908
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-02-16 17:52:42 +0000

    playing with v-for

--- app.js
+++ app.js
@@ -2,29 +2,18 @@ const app = Vue.createApp({
     data(){
         return {
             showBooks: true,
-            title: "The Final Empire",
-            age: 45,
-            author: "Brandon Sanderson",
-            x:0,
-            y:0
+            books: [
+                {title: "Name of the Wind", author: 'Patrick Rothfuss'},
+                {title: "An Alloy of Law", author: 'Brandon Sanderson'},
+                {title: "Mistborn", author: 'Brandon Sanderson'}
 
+            ]
         }
     },
     methods:{
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
-        handleEvent(ev,data) {
-            console.log(ev,ev.type)
-            if (data){
-                console.log(data)
-            }
-        },
-        handleMouseMove(e){
-            this.x = e.offsetX
-            this.y = e.offsetY
-
-        }
      
     }
     
