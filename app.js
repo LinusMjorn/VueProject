bd4ad7200c21bc404318420225a2d2b9bab7f1b7
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-02-16 17:10:17 +0000

    playing with mouse events

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const app = Vue.createApp({
     data(){
         return {
+            showBooks: true,
             title: "The Final Empire",
             age: 45,
-            author: "Brandon Sanderson"
+            author: "Brandon Sanderson",
+            x:0,
+            y:0
 
         }
     },
     methods:{
-        changeTitle(title){
-            this.title = title
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(ev,data) {
+            console.log(ev,ev.type)
+            if (data){
+                console.log(data)
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+
         }
+     
     }
     
 })
