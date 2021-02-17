e4820d3f220512a351444cd21cc53c84992ab179
Linus Mjornstedt <linusmjorn@linuss-mbp-2.home>
2021-02-17 10:37:46 +0000

    playing with dynamic classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: "http://www.google.com", 
             showBooks: true,
             books: [
-                {title: "Name of the Wind", author: 'Patrick Rothfuss', img:'assets/1.jpg'},
-                {title: "An Alloy of Law", author: 'Brandon Sanderson', img:'assets/2.jpg'},
-                {title: "Mistborn", author: 'Brandon Sanderson', img:'assets/3.jpg'}
+                {title: "Name of the Wind", author: 'Patrick Rothfuss', img:'assets/1.jpg', isFav: true},
+                {title: "The Way of Kings", author: 'Brandon Sanderson', img:'assets/2.jpg', isFav: false},
+                {title: "The Final Empire", author: 'Brandon Sanderson', img:'assets/3.jpg', isFav: true}
 
             ]
         }
